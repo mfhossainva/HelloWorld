6a8f9608316639266ec74f6e1fb18e2ab9d2736f
mfhoss <mfhoss@yahoo.com>
2018-07-21 11:52:15 -0400

    Update HelloWorld.java

--- src/com/srccodes/example/HelloWorld.java
+++ src/com/srccodes/example/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	/**
 	 * @param args - Test and test and test and test
 	 * Test again test and test again by azad
-	 * Test Helloworldazad 
+	 * Test Helloworldazad -- today's test is done,
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
